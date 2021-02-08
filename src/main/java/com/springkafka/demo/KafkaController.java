24828005570ec38b7b49f05bec5fb0a487d94743
Diego Rodrigues <diego_r.g@hotmail.com>
2021-02-08 16:54:15 +0100

    refactoring

--- src/main/java/com/springkafka/demo/KafkaController.java
+++ src/main/java/com/springkafka/demo/KafkaController.java
@@ -1,7 +1,6 @@
 package com.springkafka.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,6 @@ public class KafkaController {
 	@PostMapping("/publish")
 	public void writeMessageToTopic(@RequestParam("message") String message) {
 		producer.writeMessage(message);
-	}
-	
-	@GetMapping("/teste")
-	public String teste() {
-		return "Funcionou!!";
 	}
 
 }
